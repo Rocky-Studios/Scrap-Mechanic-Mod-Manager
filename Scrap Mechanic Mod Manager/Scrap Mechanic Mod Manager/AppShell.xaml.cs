61d2f1e1abc71c51653cdee6523d0aba4c51a1f8
RS-GoogleCoder <gergo.kaszas@outlook.com>
2023-07-24 12:45:28 +1000

    delete trash

--- Scrap Mechanic Mod Manager/Scrap Mechanic Mod Manager/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Scrap_Mechanic_Mod_Manager;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- Scrap Mechanic Mod Manager/Scrap Mechanic Mod Manager/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Scrap_Mechanic_Mod_Manager;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
