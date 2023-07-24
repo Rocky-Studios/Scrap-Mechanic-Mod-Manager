6a9a842f49314dbed4fed2f5abc4aface978c418
RS-GoogleCoder <gergo.kaszas@outlook.com>
2023-07-24 12:19:47 +1000

    project init

---
+++ Scrap Mechanic Mod Manager/Scrap Mechanic Mod Manager/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Scrap_Mechanic_Mod_Manager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Scrap Mechanic Mod Manager/Scrap Mechanic Mod Manager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Scrap_Mechanic_Mod_Manager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
